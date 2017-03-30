94b5786b7ee8122fc25ee6eaf20d1a31994e46dd
Alex <alexlewis@hotmail.com.au>
2017-03-31 00:17:31 +1100

    begin excercise 3 attempt

--- week3/exercise3.py
+++ week3/exercise3.py
@@ -28,7 +28,31 @@ def advancedGuessingGame():
     Remember to think modular. Try to keep your functions small and single
     purpose if you can!
     """
-    pass
+    print("\nwelcome to the guessing game!")
+    print("A number between 0 and _ ?")
+    upperBound = raw_input("Enter an upper bound: ")
+    lowerBound = raw_input("Enter a lower bound: ")
+    print("OK then, a number between {} and {} ?"
+          .format(lowerBound, upperBound))
+    upperBound = int(upperBound)
+    lowerBound = int(lowerBound)
+    actualNumber = random.randint(lowerBound, upperBound)
+
+    guessed = False
+
+    while not guessed:
+        guessedNumber = raw_input("guess a number: ")
+        not_number_rejector(guessedNumber)
+        guessedNumber = int(guessedNumber)
+        print("you guessed {},".format(guessedNumber),)
+        if guessedNumber == actualNumber:
+            print("you got it!! It was {}".format(actualNumber))
+            guessed = True
+        elif guessedNumber < actualNumber:
+            print("too small, try again ")
+        else:
+            print("too big, try again   ")
+    return "You got it!"
 
 
 if __name__ == "__main__":
